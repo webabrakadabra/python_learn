7caddc8735ba226bf3d8e36bbfcb13c2924a973f
grey <hireshnik@gmail.com>
2021-07-22 11:37:42 +0300

    change .gitignore

--- annotation_0.py
+++ annotation_0.py
@@ -1,6 +1,14 @@
+"""
+Анотації потрібні для підвищення інформативності кода.
+За допомогою сторонніх плагінів типу mypy
+можно аналізувати код та відловлювати помилки
+"""
+
+
 def calculate_price(unit_price: float, quantity: int) -> float:
     return unit_price * quantity
 
 
 if __name__ == '__main__':
     print(calculate_price(5, 2))
+
