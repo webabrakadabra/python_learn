cb9445515de583ba207e1f57f94e5837163d9c18
grey <hireshnik@gmail.com>
2021-08-02 16:28:50 +0300

    change .gitignore

--- classes.py
+++ classes.py
@@ -3,7 +3,7 @@
 
 class Dog:
     """class Dog"""
-    def __init__(self, age, name):
+    def __init__(self, age, name): # __init__ - конструктор класа, метод визиваєтья при створенні екз. класа
         self.age = age
         self.name = name
         print(age, name)
