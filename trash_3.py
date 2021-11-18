81a79aa2b18dc287caf94e4230fa03e8432fd8df
grey <hireshnik@gmail.com>
2021-11-18 16:17:04 +0200

    change

---
+++ trash_3.py
@@ -0,0 +1,15 @@
+class SoccerPlayer:
+    def __init__(self, name, surname):
+        self.name = name
+        self.surname = surname
+        self.goals = 0
+        self.assists = 0
+
+    def score(self, goal=1):
+        self.goals += goal
+
+    def make_assist(self, pas=1):
+        self.assists += pas
+
+    def statistics(self):
+        print(f'{self.surname} {self.name} - голы: {self.goals}, передачи: {self.assists}')
