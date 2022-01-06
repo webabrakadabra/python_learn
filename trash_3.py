759e108b308a86014ce3ff33e47825d80d6e0f5c
grey <hireshnik@gmail.com>
2022-01-06 20:45:23 +0200

    change

--- trash_3.py
+++ trash_3.py
@@ -1,16 +1,15 @@
-class SoccerPlayer:
-    def __init__(self, name, surname):
-        self.name = name
-        self.surname = surname
-        self.goals = 0
-        self.assists = 0
+class UserMail:
+    def __init__(self, login, email):
+        self.login = login
+        self.__email = email
 
-    def score(self, goal=1):
-        self.goals += goal
+    def get_email(self):
+        return self.__email
 
-    def make_assist(self, pas=1):
-        self.assists += pas
-
-    def statistics(self):
-        print(f'{self.surname} {self.name} - голы: {self.goals}, передачи: {self.assists}')
+    def set_email(self, email):
+        if email.count("@") == 1 and "." in email[email.index("@"):]:
+            self.__email = email
+        else:
+            print("Ошибочная почта")
+    email = property(fget=get_email, fset=set_email)
 
--- trash_4.py
+++ trash_4.py
@@ -1,10 +1,6 @@
-class Zebra:
-    count = 0
+import re
+a = "01/01/2222"
 
-    def which_stripe(self):
-        if self.count % 2 == 0:
-            print("Полоска белая")
-        else:
-            print("Полоска черная")
-        self.count += 1
+if re
+
 
