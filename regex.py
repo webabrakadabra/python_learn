926289d2f60971ece6aba703a5370f913fc44c33
grey <hireshnik@gmail.com>
2022-01-10 14:28:06 +0200

    ff

--- regex.py
+++ regex.py
@@ -1,5 +1,3 @@
-import re
-
 ### matсh - метод шукає по заданому pattern на початку str. Якщо не знайдено метод поверне None
 ### r - row(сирий), це означаеє, що в pattern можно використовувати \. Також якщо перед str вказати r"Disk d:\notme.py"
 ### символ слеш можна не екранувати.
@@ -35,3 +33,36 @@ print(re.search("a.b", "a+b"))  #<re.Match object; span=(0, 3), match='a+b'>
 print(re.search("a.b", "ab"))  #None
 print(re.search("a.b.c", "a-b=c"))  #<re.Match object; span=(0, 5), match='a-b=c'>
 print(re.search("a.b.c", "a-bc"))  #None
+
+### \d - означає одну любу цифру від 0 до 9
+
+print(re.search("\d", "a1"))  #<re.Match object; span=(1, 2), match='1'>
+print(re.search("\d\d", "11a"))  #<re.Match object; span=(0, 2), match='11'>
+print(re.search("a\d", "a1"))  #<re.Match object; span=(0, 2), match='a1'>
+print(re.search("a\d", "ab1"))  #None
+
+### \s - пробіл, tab, \n
+print(re.search("\s", " "))  #<re.Match object; span=(0, 1), match=' '>
+print(re.search("\s", "1"))  #None
+
+### \w - букви та _
+print(re.search("\w", "sadfas"))  #<re.Match object; span=(0, 1), match='s'>
+print(re.search("\w", "_")  #<re.Match object; span=(0, 1), match='_'>
+
+### \D - все що завгодно, але не \d(цифра)
+print(re.search("\D", "@@"))  #<re.Match object; span=(0, 1), match='@'>
+print(re.search("\D", "1"))  #None
+
+### \S - любий непробельний символ
+print(re.search("\S","ава"))  #<re.Match object; span=(0, 1), match='а'>
+print(re.search("\S","\t"))  #None
+print(re.search("\S"," "))  #None
+
+###  \W - все окрім букв, цифр та підкр.
+print(re.search("\S","#"))  #<re.Match object; span=(0, 1), match='#'>
+print(re.search("\W","1"))  #None
+print(re.search("\W","_"))  #None
+print(re.search("\W","ю"))  #None
+print(re.search("\W","\n"))  #<re.Match object; span=(0, 1), match='\n'>
+
+
